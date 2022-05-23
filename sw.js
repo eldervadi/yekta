6dbe7339d681b94cdac262a91e9a7da8ae1d11d6
arsen <eldervadi@yandex.com>
2022-05-23 14:57:10 +0430

    aa

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220523.1343';
+const cacheName = 'chirpy-20220523.1456';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
